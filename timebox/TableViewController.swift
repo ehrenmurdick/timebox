8004338889f06294a1727a3bd9ea7c2884393d1c
Ehren Murdick <pair+emurdick@pivotallabs.com>
2016-04-06 22:42:08 -0700

    last row adds a timer

--- timebox/TableViewController.swift
+++ timebox/TableViewController.swift
@@ -25,31 +25,36 @@ class TableViewController: UITableViewController, UITextFieldDelegate {
     // MARK: - Table view data source
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
+        return 2
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return timers.count
+        switch section {
+        case 0: return timers.count
+        case 1: return 1
+        default: return 0
+        }
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCellWithIdentifier("timer") as? TimerCell else {
-            fatalError("no cell found with identifier 'timer'")
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView.dequeueReusableCellWithIdentifier("timer") as? TimerCell else {
+                fatalError("no cell found with identifier 'timer'")
+            }
+            
+            let timer = timers[indexPath.row]
+            
+            cell.configureWithTimer(timer, tag: indexPath.row)
+            
+            return cell
+        default:
+            return tableView.dequeueReusableCellWithIdentifier("addCell")!
         }
-        
-        let timer = timers[indexPath.row]
-        
-        cell.configureWithTimer(timer, tag: indexPath.row)
-        
-        return cell
     }
     
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
+        return indexPath.section == 0
     }
     
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
